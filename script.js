54e16a846c74b15a412db7cf6b798a3e6fed635b
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-07-09 15:49:56 +0530

    feat - Adding distinct users

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ let sendBtn = document.getElementById("send-button");
 
 const socket = io("http://localhost:3000");
 
+let username = Math.floor(Math.random() * 100).toString();
+appendMessage("You joined the chat", "right");
+
+socket.emit("new-user", username);
+
 socket.on("chat-message", (data) => {
-  console.log(data);
-  appendMessage(data, "left");
+  appendMessage(data.user + ": " + data.message, "left");
+});
+
+socket.on("user-connected", (user) => {
+  appendMessage(user + " joined the chat", "left");
+});
+socket.on("user-disconnected", (user) => {
+  appendMessage(user + " left the chat", "left");
 });
 
 sendBtn.addEventListener("click", (e) => {
   e.preventDefault();
   socket.emit("send-chat-message", messageInput.value);
-  appendMessage(messageInput.value, "right");
+  appendMessage(`You: ${messageInput.value}`, "right");
   console.log(messageInput.value);
   messageInput.value = "";
 });
--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const io = require("socket.io")(3000, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected");
-  socket.emit("chat-message", "Let's fight a battle!");
+let users = {};
 
+io.on("connection", (socket) => {
+  socket.on("new-user", (user) => {
+    users[socket.id] = user;
+    socket.broadcast.emit("user-connected", user);
+  });
   socket.on("send-chat-message", (message) => {
-    console.log(message);
-    socket.broadcast.emit("chat-message", message);
+    console.log(`${users[socket.id]}: ${message}`);
+    socket.broadcast.emit("chat-message", { user: users[socket.id], message });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
   });
 });
