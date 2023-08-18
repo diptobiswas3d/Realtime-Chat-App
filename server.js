3be2cc4477930700992632b2df99541c9f2b0c1c
diptobiswasanime4 <diptobiswasanime4@gmail.com>
2023-08-18 19:39:21 +0530

    feat - features

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const io = require("socket.io")(3000, {
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server, {
   cors: {
     origin: "*",
   },
 });
+
+const PORT = 3000 || process.env.PORT;
 
 let users = {};
 
@@ -19,4 +28,12 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("user-disconnected", users[socket.id]);
     delete users[socket.id];
   });
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Neo Chat App");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
 });
